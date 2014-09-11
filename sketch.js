df9d0047c9edeb46b3ccc8a74b979c95d2d60f03
parkjiyun <jyun38@gmail.com>
2014-09-11 16:01:07 +0900

    fawfwaef

--- sketch.js
+++ sketch.js
@@ -1,26 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
+  noFill();
 
   stroke(255, 0, 0);
-/*
-  var x;
 
-  x = 100;
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 250;
-  ellipse(x * 150, 100, 50, 50);
-
-  x = 300;
-  ellipse(x * 150, 100, 50, 50);
-  */
+}
 
 function draw () {
-for (var x = 1; x <=8; x++)
+
+   background(128, 0, 64);
+
+for (var x = 1; x <= 8; x++)
 {
-  ellipse(x * 70, x * 20 + 100, 10 + x * 3, 20 + x * 10);
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
 }
-var size = Math, abs(mouseX - 320);
-ellipse(320, 240, size, size);
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
