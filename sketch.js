1e9e12de9bcdfed73b5e6627b53ffb7035722386
parkjiyun <jyun38@gmail.com>
2014-09-11 15:24:55 +0900

    four

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 function setup() {
   createCanvas(640, 480);
   background(128, 0, 64);
-  noFill();
 
   stroke(255, 0, 0);
 /*
@@ -16,6 +15,8 @@ function setup() {
   x = 300;
   ellipse(x * 150, 100, 50, 50);
   */
+
+
 for (var x = 1; x <=3; x++)
 {
   ellipse(x * 100, 100, 50, 50);
